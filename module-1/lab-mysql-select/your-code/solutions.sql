fed10dc47cbe81fa93c30f538593ba25b31a1b62
Javier Díaz <jdcaracuel@gmail.com>
2019-06-18 22:00:50 +0200

    desafío 3 superado

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -7,7 +7,31 @@ USE publications;
 SELECT authors.au_id AS 'AUTHORS ID',
 	authors.au_lname AS 'LAST NAME',
 	authors.au_fname AS 'FIRST NAME', 
-	titles.title AS 'TITLE'
+	titles.title AS 'TITLE',
+    publishers.pub_name AS 'PUBLISHER'
+    
+FROM authors 
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers
+ON titles.pub_id = publishers.pub_id;
+
+#Desafío 2
+
+
+
+
+
+
+SELECT authors.au_id AS 'AUTHORS ID',
+	authors.au_lname AS 'LAST NAME',
+	authors.au_fname AS 'FIRST NAME', 
+	titles.title AS 'TITLE',
+	publishers.pub_name AS 'PUBLISHER',
+    
+	COUNT(*) AS 'TITLE COUNT'
     
 FROM authors 
 INNER JOIN titleauthor
@@ -17,5 +41,30 @@ ON titleauthor.title_id = titles.title_id
 INNER JOIN publishers
 ON titles.pub_id = publishers.pub_id
 
-#Desafío 2
+GROUP BY titles.title_id, authors.au_id
+ORDER BY authors.au_id DESC;
+
+#Desafío 3
+
+SELECT authors.au_id AS 'AUTHORS ID',
+	authors.au_lname AS 'LAST NAME',
+	authors.au_fname AS 'FIRST NAME', 
+
+SUM(sales.qty) AS TOTAL
+
+FROM authors 
+INNER JOIN titleauthor
+ON authors.au_id = titleauthor.au_id
+INNER JOIN titles
+ON titleauthor.title_id = titles.title_id
+INNER JOIN publishers
+ON titles.pub_id = publishers.pub_id
+INNER JOIN sales
+ON titleauthor.title_id = sales.title_id
+
+GROUP BY titles.title_id, authors.au_id
+ORDER BY TOTAL DESC
+LIMIT 3
+
+#Desafío 4
 
